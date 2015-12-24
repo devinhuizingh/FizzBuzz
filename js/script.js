45fd0cc81784729499de70f17e49fb489930ec70
devinhuizingh <devinhuizingh@gmail.com>
2015-12-24 08:45:58 -0700

    another change

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ $(document).ready(function() {
 	$( "#button" ).click(function() {
               
 		for(var i = 1; i <= 100; i++) {
-		   var printVal = '';
-		   if(i % 3 == 0) printVal = 'fizz';
-		   if(i % 5 == 0) printVal = printVal + 'buzz';
-		   if(printVal == '') printVal = i;
-		   console.log(printVal);
-		}   
+			
+			if (i%3 ==0 && i%5 ==0) {
+				console.log("fizzbuzz")
+			}
+			else if (i%3 == 0) {
+				console.log("fizz")
+				}
+			else if (i%5 == 0) {
+				console.log("buzz")
+				}
+			
+
+			else console.log(i);
+
+		}
 	
 
 	})
