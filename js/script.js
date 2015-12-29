03ef2974ba070b767a96681d5b3d373c93ee12ee
devinhuizingh <devinhuizingh@gmail.com>
2015-12-29 15:08:30 -0700

    added a max number

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
 
-	$( "#button" ).click(function() {
-              
-		for(var i = 1; i <= 100; i++) {
-			
-			if (i%3 ==0 && i%5 ==0) {
-				console.log("fizzbuzz")
-			}
-			else if (i%3 == 0) {
-				console.log("fizz")
-				}
-			else if (i%5 == 0) {
-				console.log("buzz")
-				}
-			
 
-			else console.log(i);
-
-		}
 	
+	
+	$( "button" ).click(function(e) {
+        e.preventDefault();
+
+       	for(var i = "0"; i <= $('input#starting').val(); i++) {
+		
+		if (i%3 ==0 && i%5 ==0) {
+			console.log("fizzbuzz")
+		}
+		else if (i%3 == 0) {
+			console.log("fizz")
+			}
+		else if (i%5 == 0) {
+			console.log("buzz")
+			}
+		
+
+		else console.log(i);
+			}
 
 	})
-
-
+	
 })
